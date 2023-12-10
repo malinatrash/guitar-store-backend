af5393a875b6d8c624ca932251f371284eb781f4
Pavel <malinatrashh@gmail.com>
2023-12-11 04:09:47 +0800

    table

--- backend_api/migrations/Views/ShoppingCartAPIView.py
+++ backend_api/migrations/Views/ShoppingCartAPIView.py
@@ -27,6 +27,7 @@ class ShoppingCartAPIView(APIView):
 
             for cart in carts:
                 product_id = cart.product_id_id
+                count = cart.products_count
                 product = Product.objects.get(product_id=product_id)
                 product_data = {
                     'product_id': product.product_id,
@@ -38,10 +39,10 @@ class ShoppingCartAPIView(APIView):
                     'country_of_origin': product.country_of_origin.name,
                     'year_of_production': product.year_of_production,
                     'image_url': product.image_url,
+                    'total_price': count * product.price,
                 }
                 products_data.append(product_data)
 
-            # Возвращение данных о товарах в ответе
             response_data = {
                 'products': products_data,
                 'status_code': status.HTTP_200_OK
@@ -49,10 +50,10 @@ class ShoppingCartAPIView(APIView):
             return Response(response_data, status=status.HTTP_200_OK)
 
         except Exception as e:
-            # Логирование информации об ошибке
+
             logger.error(f"An error occurred: {str(e)}")
             print(str(e))
-            # Обработка ошибок, если что-то идет не так
+
             response_data = {
                 'message': 'An error occurred.',
                 'status_code': status.HTTP_500_INTERNAL_SERVER_ERROR
@@ -80,7 +81,7 @@ class ShoppingCartAPIView(APIView):
                 }
                 return Response(response_data, status=status.HTTP_404_NOT_FOUND)
 
-            if quantity < 0:
+            if quantity <= 0:
                 raise ValidationError("Quantity should be a positive integer.")
 
             existing_cart_item = ShoppingCart.objects.filter(
